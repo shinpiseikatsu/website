099b9a2f7ccb7ad5d8ec0696f70173e4b8d6912a
si-zerodarkthirty <kyotan.equilibria@gmail.com>
2020-12-20 13:59:33 +0900

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,11 +74,13 @@ export default {
     routes () {
       return Promise.all([
         client.getEntries({
-          'content_type': 'work'
+          'content_type': 'work',
+          'content_type': 'product'
         })
-      ]).then(([works]) => {
+      ]).then(([works,products]) => {
         return [
-          ...works.items.map(work => `work/${work.fields.slug}`)
+          ...works.items.map(work => `work/${work.fields.slug}`),
+          ...products.items.map(product => `work/${product.fields.slug}`)
         ]
       })
     }
